2307d1552ebf5216260f5268150c32f9e794e5cd
Levi Provetti <levi.provetti@gmail.com>
2021-10-30 05:49:42 -0700

    first commit

---
+++ src/br/com/dio/segundoprograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.modelo.Gata;
+
+public class segundoprograma {
+
+	public static void main(String[] args) {
+		
+		Gata gata = new Gata();
+		Livros livros = new Livros();
+		
+		System.out.println(gata);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b=b 3;
+		System.out.println("Hello World! " + (a+b));*/
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag; 
+	
+}
